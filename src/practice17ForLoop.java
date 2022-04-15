8632537fc37910b0aeb07c6d9ba5acd83e106f68
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-04-15 14:17:55 +0800

    By George

---
+++ src/practice17ForLoop.java
@@ -0,0 +1,35 @@
+public class practice17ForLoop {
+    //There are condition in For, if condition values=true then For will be loop if not then it would be dismissed
+    /*
+    * for(init statement; kondisi; post statement){
+    * //block perulangan
+    * }
+    * init statement = only once execution
+    * kondisi = pengecekan setiap perulangan, kalau true maka dieksekusi terus, kalau false maka berhenti
+    * post statement = always execute at end of the looping
+    *
+    * init statement,kondisi, post statement are optionall.. if not included kondisi then the value is true
+    ** * * * *    * */
+
+    public static void main(String[] args) {
+
+        /*1. LOOPING WITH CONDITION*/
+        var counter = 1;
+
+        for(; counter <= 10; ){
+            System.out.println("Looping number "+ counter);
+            counter++;
+        }
+        System.out.println(counter);
+
+        //Dont try this
+        //for(;;){
+        //    System.out.println("Always looping");
+        //}
+
+        /*2. LOOPING with Init statement*/
+    }
+}
+
+
+
