d059513941bf37a64518e66910b5e9329c937d27
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-04-16 17:40:21 +0800

    By George

--- src/practiceJustForTesting.java
+++ src/practiceJustForTesting.java
@@ -1,8 +1,14 @@
 public class practiceJustForTesting {
     //Bruvh I confused about modulus thing 16 April 2022, I mean, yeaa I was using that in my entire college but maybe I never use it in entire project/program xD but I'm still curious LOL
     public static void main(String[] args) {
-        var counter = 10;
-
-        System.out.println(counter % 6);
+        var tipe ="";
+        for(var counter = 1; counter <=100; counter++){
+            if(counter % 2==0){
+                tipe = "Genap";
+            } else {
+                tipe = "Ganjil";
+            }
+            System.out.println(counter + " bertipe " + tipe);
+        }
     }
 }
