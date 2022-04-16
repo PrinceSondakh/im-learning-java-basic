7a93d2e471e3abb486e87b8594174e139993ef12
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-04-16 17:30:45 +0800

    By George

---
+++ src/practiceJustForTesting.java
@@ -0,0 +1,8 @@
+public class practiceJustForTesting {
+    //Bruvh I confused about modulus thing 16 April 2022, I mean, yeaa I was using that in my entire college but maybe I never use it in entire project/program xD but I'm still curious LOL
+    public static void main(String[] args) {
+        var counter = 10;
+
+        System.out.println(counter % 6);
+    }
+}
