3b2be6c9d53218b77a3a6b12c07b77257d1be466
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-04-18 18:52:24 +0800

    By George

--- src/practice23MethodParameter.java
+++ src/practice23MethodParameter.java
@@ -1,6 +1,15 @@
 public class practice23MethodParameter {
-    //argume
+    //parameter dan argumen sama aja
+    /*
+    Fungsi parameter yaitu mengirim informasi ke method yang ingin kita panggil
+    parameter ditempatkan didalam kurung di deklarasi method
+    * cara membuat parameter sama sperti membuat variabel */
+
     public static void main(String[] args) {
+
+    }
+
+    static void sayHello (String firstName, String lastName){
 
     }
 }
---
+++ src/practice23MethodParameter2.java
@@ -0,0 +1,8 @@
+public class practice23MethodParameter2 {
+    public static void main(String[] args) {
+        testingABC("Ayam 1");
+    }
+    static void testingABC (String A){
+        System.out.println(A);
+    }
+}
