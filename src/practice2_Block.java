ffd62c99c36956ef653c34f86714f2a3c6d21ed9
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-05-05 14:52:14 +0800

    By George

--- src/practice2_Block.java
+++ src/practice2_Block.java
@@ -1,11 +1,18 @@
 public class practice2_Block {
-
+    //Block is for ? Grouping code so it would be satisfying to see
     public static void main(String[] args) {
 
         System.out.println("Hello World 1");
         System.out.println("Hello World 2");
         System.out.println("Hello World 3");
 
+        {
+
+            {
+                System.out.println("a");
+            }
+
+        }
     }
 
 }
