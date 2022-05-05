16c6ff53602f4bfbc2a977186d0a463ae932ab8b
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-05-05 14:56:48 +0800

    By George

---
+++ src/practice2_IfStatement.java
@@ -0,0 +1,13 @@
+public class practice2_IfStatement {
+    //Deeper technique
+    public static void main(String[] args) {
+
+        byte a = 70;
+        byte b = 70;
+
+        if(((a >= 70 && b >= 70)&&(a >= 70 && b >= 70))&&((a >= 70 && b >= 70)&&(a >= 70 && b >= 70))){
+            System.out.println("Selamat anda lulus");
+        }
+
+    }
+}
