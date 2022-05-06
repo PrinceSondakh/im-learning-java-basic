14e590cd99548ff83262b563bdd59ccc43d9ba31
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-05-06 16:39:45 +0800

    Bruvh i'm so confuse about practice2_RecursiveMethod like wut

---
+++ src/practice2_JustTryingsomething.java
@@ -0,0 +1,11 @@
+public class practice2_JustTryingsomething
+{
+    public static void main(String[] args) {
+
+        var a = 10_0L;
+        var c = (byte) a;
+
+        System.out.println(c);
+
+    }
+}
